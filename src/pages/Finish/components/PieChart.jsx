aeb7baa204d2241bd978240a3173ad6df817cbd4
Petro Matkovskyi <petromatkovskij3021@gmail.com>
2023-07-06 23:34:47 +0300

    changed PieCart`s styles

--- src/pages/Finish/components/PieChart.jsx
+++ src/pages/Finish/components/PieChart.jsx
@@ -6,16 +6,13 @@ const PieChart = ({ data }) => {
     const matches = useMediaQuery(theme.breakpoints.down('sm'));
 
     return (
-        <Paper>
-            <Chart
-                data={data}
-                width={matches ? 150 : 300}
-                height={matches ? 300 : 300}
-                sx={{
-                    border: '1px solid lightgreen',
-                    borderRadius: 3,
-                }}
-            >
+        <Paper
+            sx={{
+                border: '1px solid lightgreen',
+                borderRadius: 3,
+            }}
+        >
+            <Chart data={data} width={matches ? 150 : 300} height={matches ? 300 : 300}>
                 <PieSeries valueField="value" argumentField="argument" />
                 <Legend position="bottom" />
                 <Title text="Correct / Incorrect" />
